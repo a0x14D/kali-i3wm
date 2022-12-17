e8ea16fc372eb701ef3822cbd717052f9d3cebd8
aunknownwhoami <aunknown.whoami@gmail.com>
2022-12-17 20:01:26 +0530

    new change

---
+++ scripts/zsh.sh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+#!/bin/sh
+
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autocomplete
+
